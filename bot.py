8ca6001b3e80a15894239b0728cef0cd492df64b
colored-glaze <lj_sakura@qq.com>
2024-12-07 22:53:29 +0800

    改改改

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import nonebot
-from nonebot.adapters.onebot.v11 import Adapter as v11Adapter  # 避免重复命名
+from nonebot.adapters.onebot.v11 import Adapter
 
 # 初始化 NoneBot
 nonebot.init()
 
 # 注册适配器
 driver = nonebot.get_driver()
-driver.register_adapter(v11Adapter)
+driver.register_adapter(Adapter)
 
 # 在这里加载插件
 nonebot.load_builtin_plugins("echo")  # 内置插件
